ce66e3b7077c26019db740e377b44f0492c1d619
will.maddern <will.maddern@56a04c5b-9c67-3e02-b2b0-65419631fbac>
2012-03-23 01:55:42 +0000

    Completed MSCTrainer code, untested
    git-svn-id: https://openfabmap.googlecode.com/svn/branches/openfabmap2-dev@58 56a04c5b-9c67-3e02-b2b0-65419631fbac

--- src/BOWMSCTrainer.cpp
+++ src/BOWMSCTrainer.cpp
@@ -38,25 +38,53 @@ Mat BOWMSCTrainer::cluster() const
 
 Mat BOWMSCTrainer::cluster(const Mat& descriptors) const {
 
+	CV_Assert(!descriptors.empty());
+
 	vector<Mat> initialCentres;
 	vector<list<Mat> > clusters;
 	vector<list<Mat> >::iterator Ci;
 
 	double threshold = pow(clusterSize,2.0);
 
+	Mat icovar = Mat::eye(descriptors.cols,descriptors.cols,descriptors.type());
 
+	initialCentres.push_back(descriptors.rowRange(0,0));
+
+	for (int i = 1; i < descriptors.rows; i++) {
+		double minDist = DBL_MAX;
+		for (size_t j = 0; j < initialCentres.size(); j++) {
+			minDist = std::min(minDist,cv::Mahalanobis(descriptors.rowRange(i,i),initialCentres[j],icovar));
+		}
+		if (minDist < threshold)
+			initialCentres.push_back(descriptors.rowRange(i,i));
+	}
+
+	clusters.resize(initialCentres.size());
+	for (int i = 0; i < descriptors.rows; i++) {
+		int index; double dist, minDist = DBL_MAX;
+		for (size_t j = 0; j < initialCentres.size(); j++) {
+			dist = cv::Mahalanobis(descriptors.rowRange(i,i),initialCentres[j],icovar);
+			if (dist < minDist) {
+				minDist = dist;
+				index = j;
+			}
+		}
+		clusters[index].push_back(descriptors.rowRange(i,i));
+	}
+
+	Mat vocabulary;
+	Mat centre = Mat::zeros(1,descriptors.cols,descriptors.type());
+	for (size_t i = 0; i < clusters.size(); i++) {
+		centre.setTo(0);
+		for (list<Mat>::iterator Ci = clusters[i].begin(); Ci != clusters[i].end(); Ci++) {
+			centre += *Ci;
+		}
+		centre /= (double)clusters[i].size();
+		vocabulary.push_back(centre);
+	}
 
 	return vocabulary;
 }
-
-
-
-
-
-
-
-
-
 
 }
 
