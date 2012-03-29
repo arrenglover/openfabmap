f5934f0727f7f3b63952e861c8c03b5d390f94ed
will.maddern <will.maddern@56a04c5b-9c67-3e02-b2b0-65419631fbac>
2012-03-29 00:45:37 +0000

    Added proper preamble to file headers
    git-svn-id: https://openfabmap.googlecode.com/svn/branches/openfabmap2-dev@71 56a04c5b-9c67-3e02-b2b0-65419631fbac

--- src/BOWMSCTrainer.cpp
+++ src/BOWMSCTrainer.cpp
@@ -1,9 +1,28 @@
-/*
- * BOWMSCTrainer.cpp
- *
- *  Created on: 22/03/2012
- *      Author: will
- */
+/*------------------------------------------------------------------------
+Copyright 2012 Arren Glover [aj.glover@qut.edu.au]
+			   Will Maddern [w.maddern@qut.edu.au]
+
+This file is part of OpenFABMAP. http://code.google.com/p/openfabmap/
+
+OpenFABMAP is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+OpenFABMAP is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+For published work which uses all or part of OpenFABMAP, please cite:
+http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=5509547&tag=1
+
+Original Algorithm by Mark Cummins and Paul Newman:
+http://ijr.sagepub.com/content/27/6/647.short
+
+You should have received a copy of the GNU General Public License along with
+OpenFABMAP. If not, see http://www.gnu.org/licenses/.
+------------------------------------------------------------------------*/
 
 #include "../include/openfabmap.hpp"
 
